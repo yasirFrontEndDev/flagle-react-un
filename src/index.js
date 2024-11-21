d92680608d535351022753b9906d311bd082bd5f
Yasir Ali Khan <pukhtoon56@gmail.com>
2024-11-22 00:28:35 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route ,HashRouter  } from 'react-router-dom';
 import App from './App';
 import HomePage from './components/HomePage';
 import reportWebVitals from './reportWebVitals';
@@ -8,14 +8,13 @@ import UnlimitedMode from './UnlimitedMode';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/daily" element={<App />} />
         <Route path="/unlimited" element={<UnlimitedMode />} />
-
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
